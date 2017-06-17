9a7e1b6bad1e2fb3626259306ca97368263a2b54
golabmateusz <mateusz.golab@onet.eu>
2017-06-18 00:35:37 +0200

    pliki cpp oraz h na podstawie class diagram

---
+++ Biblioteka_PO2017/Biblioteka_PO2017.cpp
@@ -0,0 +1,16 @@
+// Biblioteka_PO2017.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "abstrakcyjnafabrykaosob.h"
+#include "fabrykabibliotekarzy.h"
+#include "fabrykauzytkownikow.h"
+#include "osoba.h"
+#include "bibliotekarz.h"
+#include "uzytkownik.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
